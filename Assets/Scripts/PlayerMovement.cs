f08cc5f0d7157c477987fad59ba5c3f96acaaece
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-02 23:09:42 +0700

    Demine
    2/04/2024

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,20 @@ public class PlayerMovement : MonoBehaviour
     public float _moveSpeed = 5;
 
     private Rigidbody2D _rb;
+    float horizontalInput = Input.GetAxisRaw("Horizontal");
+
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+    }
+
     private void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
 
         Vector3 moveInput = new Vector3(horizontalInput, 0, 0);
     }
